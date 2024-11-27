11412e786d5cd1f6b444522f44e64972b0525388
kestsaurav21 <kestsaurav21@outlook.com>
2024-11-28 02:00:04 +0530

    Feature: update landing-page

--- src/components/signupForm.jsx
+++ src/components/signupForm.jsx
@@ -1,111 +1,242 @@
-import { useState } from "react"
+import { useState } from "react";
+
 export default function SignupForm() {
-    const [formData, setFormData] = useState({
-        name: "",
-        email: "",
-        mobile: "",
-        userName: "",
-        termsAndConditions: false,
-    })
-    const [error, setError] = useState({
-        name: false,
-        email: false,
-        mobile: false,
-        userName: false,
-        termsAndConditions: false,
-    })
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        const { name, email, mobile, userName, termsAndConditions } = formData
-        setError({
-            name: false,
-            email: false,
-            mobile: false,
-            userName: false,
-            termsAndConditions: false,
-        })   // reset error state
-        if (name.trim().length === 0) {  //        
-            setError((prevState) => { return { ...prevState, name: true } })
-        }
-        if (email.trim().length === 0) {
-            setError((prevState) => { return { ...prevState, email: true } })
-        }
-        if (mobile.trim().length === 0) {
-            setError((prevState) => { return { ...prevState, mobile: true } })
-        }
-        if (userName.trim().length === 0) {
-            setError((prevState) => { return { ...prevState, userName: true } })
-        }
-        if (termsAndConditions === false) {
-            setError((prevState) => { return { ...prevState, termsAndConditions: true } })
-        }
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    mobile: "",
+    userName: "",
+    termsAndConditions: false,
+  });
+  const [error, setError] = useState({
+    name: false,
+    email: false,
+    mobile: false,
+    userName: false,
+    termsAndConditions: false,
+  });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { name, email, mobile, userName, termsAndConditions } = formData;
+    setError({
+      name: false,
+      email: false,
+      mobile: false,
+      userName: false,
+      termsAndConditions: false,
+    }); // reset error state
+    if (name.trim().length === 0) {
+      //
+      setError((prevState) => {
+        return { ...prevState, name: true };
+      });
     }
-    return (
-        <div>
-            <h1 style={{
-                color: "white",
-            }}>Signup Form</h1>
-            <form style={{
-                display: "flex",
-                flexDirection: "column",
-                width: "80%",
-                margin: "0 auto",
-                gap: "10px",
-            }}
-                onSubmit={handleSubmit}
-            >
-                <input placeholder="Name" type="text" onChange={(e) => setFormData({ ...formData, name: e.target.value })} value={formData.name} />
-                {error.name && <p style={{
-                    color: "red",
-                    fontSize: "12px",
-                    margin: "0",
-                }}>Name is required</p>}
-                <input placeholder="Email" type="text" onChange={(e) => setFormData({ ...formData, email: e.target.value })} value={formData.email} />
-                {error.email && <p style={{
-                    color: "red",
-                    fontSize: "12px",
-                    margin: "0",
-                }}>Email is required</p>}
-                <input placeholder="Mobile" type="text" onChange={(e) => setFormData({ ...formData, mobile: e.target.value })} value={formData.mobile} />
-                {error.mobile && <p style={{
-                    color: "red",
-                    fontSize: "12px",
-                    margin: "0",
-                }}
-                >Mobile is required</p>}
-                <input placeholder="Username" type="text" onChange={(e) => setFormData({ ...formData, userName: e.target.value })} value={formData.userName} />
-                {error.userName && <p style={{
-                    color: "red",
-                    fontSize: "12px",
-                    margin: "0",
-                }}>Username is required</p>}
-                <div style={{
-                    display: "flex",
-                    gap: "10px",
-                }}>
-                    <input id="checkbox" type="checkbox" onChange={(e) => setFormData({ ...formData, termsAndConditions: e.target.checked })} checked={formData.termsAndConditions} />
-                    <label style={{
-                        color: "white",
-                    }} htmlFor="checkbox">Share my registration data with Superapp</label>
-                </div>
-                {error.termsAndConditions && <p style={{
-                    color: "red",
-                    fontSize: "12px",
-                    margin: "0",
-                }}>You must agree to the terms and conditions</p>}
-                <button style={{
-                    width: "120px",
-                }} type="submit">Sign Up</button>
-            </form>
+    if (email.trim().length === 0) {
+      setError((prevState) => {
+        return { ...prevState, email: true };
+      });
+    }
+    if (mobile.trim().length === 0) {
+      setError((prevState) => {
+        return { ...prevState, mobile: true };
+      });
+    }
+    if (userName.trim().length === 0) {
+      setError((prevState) => {
+        return { ...prevState, userName: true };
+      });
+    }
+    if (termsAndConditions === false) {
+      setError((prevState) => {
+        return { ...prevState, termsAndConditions: true };
+      });
+    }
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "518px",
+      }}>
+      {/* Header */}
+      <h1
+        style={{
+          color: "#72DB73",
+          fontFamily: "Single Day",
+          fontSize: "47px",
+          marginBottom: "10px",
+        }}>
+        Super App
+      </h1>
+      <p
+        style={{
+          color: "white",
+          fontFamily: "DM Sans",
+          fontSize: "18px",
+          marginBottom: "20px",
+        }}>
+        Create your new account
+      </p>
+
+      {/* Form */}
+      <form
+        onSubmit={handleSubmit}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "10px",
+        }}>
+        <input
+          placeholder="Name"
+          type="text"
+          onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+          value={formData.name}
+          style={{
+            background: "#292929",
+            padding: "0.8rem",
+            color: "white",
+            border: "none",
+            borderRadius: "5px",
+          }}
+        />
+        {error.name && <ErrorText text="Name is required" />}
+
+        <input
+          placeholder="Email"
+          type="email"
+          onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+          value={formData.email}
+          style={{
+            background: "#292929",
+            padding: "0.8rem",
+            color: "white",
+            border: "none",
+            borderRadius: "5px",
+          }}
+        />
+        {error.email && <ErrorText text="Email is required" />}
+
+        <input
+          placeholder="Mobile"
+          type="text"
+          onChange={(e) => setFormData({ ...formData, mobile: e.target.value })}
+          value={formData.mobile}
+          style={{
+            background: "#292929",
+            padding: "0.8rem",
+            color: "white",
+            border: "none",
+            borderRadius: "5px",
+          }}
+        />
+        {error.mobile && <ErrorText text="Mobile is required" />}
+
+        <input
+          placeholder="Username"
+          type="text"
+          onChange={(e) =>
+            setFormData({ ...formData, userName: e.target.value })
+          }
+          value={formData.userName}
+          style={{
+            background: "#292929",
+            padding: "0.8rem",
+            color: "white",
+            border: "none",
+            borderRadius: "5px",
+          }}
+        />
+        {error.userName && <ErrorText text="Username is required" />}
+
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: "10px",
+            padding: "10px 0",
+          }}>
+          <input
+            id="checkbox"
+            type="checkbox"
+            onChange={(e) =>
+              setFormData({ ...formData, termsAndConditions: e.target.checked })
+            }
+            checked={formData.termsAndConditions}
+            style={{ margin: 0 }}
+          />
+          <label
+            htmlFor="checkbox"
+            style={{
+              fontFamily: "DM Sans",
+              fontSize: "14px",
+              color: "#7C7C7C",
+            }}>
+            Share my registration data with Superapp
+          </label>
         </div>
-    )
+        {error.termsAndConditions && (
+          <ErrorText text="You must agree to the terms and conditions" />
+        )}
+
+        <button
+          type="submit"
+          style={{
+            background: "#72DB73",
+            color: "white",
+            width: "100%",
+            padding: "0.8rem",
+            fontFamily: "Roboto",
+            fontSize: "20px",
+            fontWeight: "600",
+            cursor: "pointer",
+            borderRadius: "30px",
+          }}>
+          SIGN UP
+        </button>
+      </form>
+      {/* Terms */}
+      <div
+        style={{
+          width: "292px",
+        }}>
+        <p
+          style={{
+            marginTop: "15px",
+            fontFamily: "DM Sans",
+            fontSize: "10px",
+            color: "#7C7C7C",
+          }}>
+          By clicking on Sign up, you agree to Superapp{" "}
+          <a href="#" style={{ color: "#72DB73", textDecoration: "none" }}>
+            Terms and Conditions of Use
+          </a>
+          . <br />
+          To learn more about how Superapp collects, uses, and protects your
+          data, please read the{" "}
+          <a href="#" style={{ color: "#72DB73", textDecoration: "none" }}>
+            Privacy Policy
+          </a>
+          .
+        </p>
+      </div>
+    </div>
+  );
 }
 
-//HOMEWORK
-
-// (mandatory)
-// put image on left side of signup form
-// on right side, match colors and texts for form
-
-//(optional)
-// find out what is a github pull request
+const ErrorText = ({ text }) => {
+  return (
+    <p
+      style={{
+        color: "red",
+        fontSize: "14px",
+        margin: "0",
+        textAlign: "left",
+      }}>
+      {text}
+    </p>
+  );
+};
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,21 +1,34 @@
 import SignupForm from "../components/signupForm"
+import image from "../assets/image.png"
 export default function Home() {
     return (
         <div style={{
+            
             display: "flex",
         }}>
             <div style={{
                 background: "cyan",
                 height: "100vh",
                 width: "50vw"
-            }}></div>
+            }}>
+                <img style={{
+                    height: "100%",
+                    width: "100%",
+                    objectFit: "cover",
+                }} src={image} alt="image" />
+            </div>
             <div style={{
                 background: "black",
                 height: "100vh",
-                width: "50vw"
+                width: "50vw",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                
             }}>
                 <SignupForm />
             </div>
         </div>
     )
 }
+

bc4fdb3bd4bd231bb2b59f27e45760ebe5ca3e96
kestsaurav21 <kestsaurav21@outlook.com>
2024-11-28 03:18:25 +0530

    fix: home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -16,6 +16,20 @@ export default function Home() {
                     width: "100%",
                     objectFit: "cover",
                 }} src={image} alt="image" />
+                <p
+                    style={{
+                        position: "absolute",
+                        bottom: "50px",
+                        left: "30px",
+                        color: "white",
+                        fontFamily: "DM Sans",
+                        fontSize: "36px",
+                        fontWeight: "bold",
+                        margin: 0,
+                    }}
+                >
+                    Discover new things on <br /> Superapp
+                </p>
             </div>
             <div style={{
                 background: "black",

b80cc10d5f8fbf26fbd7a15c1834f5cef55139c7
Soni <soniagrawal0010@gmail.com>
2024-11-30 21:05:36 +0530

    addingfiles

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
-import Movies from './pages/movies'
+import Movie from './pages/Movie'
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Movies />} />
+        <Route path="/" element={<Home/>} />
+        <Route path="/movies" element={<Movie/>} />
       </Routes>
     </BrowserRouter>
   )
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,20 +1,19 @@
-
-import dance from '../Image/Dance.png'
-import './Home.css'
-import Signup from '../components/Signup'
+import dance from "../Image/Dance.png";
+import "./Home.css";
+import Signup from "../components/Signup";
 function Home() {
   return (
-    <div className='home'>
-      <div className='left'>
+    <div className="home">
+      <div className="left">
         <img src={dance} />
-        <div className='heading'>
+        <div className="heading">
           <h1>Discover new things on Superapp</h1>
         </div>
       </div>
-      <div className='right'>
+      <div className="right">
         <Signup />
       </div>
     </div>
-  )
+  );
 }
-export default Home
+export default Home;

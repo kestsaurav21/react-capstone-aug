655eb713be775ea7a3c6cda7cabb51b5ff7b0518
ishar19 <isharjain19@gmail.com>
2024-12-02 22:33:16 +0530

    feat: fourth page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
 import Movie from './pages/Movie'
+import Recommendation from './pages/recommendation'
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/movies" element={<Movie/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movie />} />
+        <Route path="/recommendation" element={<Recommendation />} />
       </Routes>
     </BrowserRouter>
   )
--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import { useState } from "react";
 import MovieGrid from "../components/MovieGrid";
 import MovieChip from "../components/movieChip";
 import './Movie.css'
@@ -11,72 +11,83 @@ import music from '../Image/Music.png'
 import romance from '../Image/Romance.png'
 import thriller from '../Image/Thriller.png'
 import western from '../Image/Western.png'
+import { useNavigate } from "react-router-dom";
 const MOVIES = [
   {
     value: "action",
     label: "Action",
     image: action,
-    background:"#FF5209"
+    background: "#FF5209"
 
   },
   {
     value: "romance",
     label: "Romance",
     image: romance,
-    background:"#D7A4FF"
+    background: "#D7A4FF"
   },
   {
     value: "drama",
     label: "Drama",
-    image: drama ,
-    background:"#148A08"
+    image: drama,
+    background: "#148A08"
   },
   {
     value: "thriller",
     label: "Thriller",
     image: thriller,
-    background:"#84C2FF"
+    background: "#84C2FF"
   },
   {
     value: "western",
     label: "Western",
     image: western,
-    background:"#902500"
+    background: "#902500"
   },
   {
     value: "horror",
     label: "Horror",
     image: horror,
-    background:"#7358FF"
+    background: "#7358FF"
   },
   {
     value: "fanstasy",
     label: "Fanstasy",
     image: fantasy,
-    background:"#FF4ADE"
+    background: "#FF4ADE"
   },
   {
     value: "music",
     label: "Music",
     image: music,
-    background:"#E61E32"
+    background: "#E61E32"
   },
   {
     value: "fiction",
     label: "Fiction",
     image: fiction,
-    background:"#6CD061"
+    background: "#6CD061"
   },
 ];
 
- function Movie() {
+function Movie() {
   const [selected, setSelected] = useState([]);
+  const navigate = useNavigate();
+  const handleNext = () => {
+    if (selected.length < 3) {
+      alert("Minimum 3 category required")
+    }
+    else {
+      localStorage.setItem("selected", JSON.stringify(selected));
+      navigate("/recommendation");
+    }
+  }
   return (
     <div className="main-movie">
       <div className="left-movie">
         <h1 className="chip-heading">Super app</h1>
         <p className="entertainment">Choose your entertainment category</p>
-        {selected.map((movie) =>(
+        {selected.map((movie) => (
           <MovieChip
             key={movie}
             selected={selected}
@@ -85,23 +96,23 @@ const MOVIES = [
           />
         ))}
         {
-          selected.length>0&&selected.length<3?<p className="catagory">Minimum 3 category required</p>:null
+          selected.length > 0 && selected.length < 3 ? <p className="catagory">Minimum 3 category required</p> : null
         }
       </div>
       <div className="right-movie">
-      <div className="right-grid">
-        {MOVIES.map((movie) => (
-          <MovieGrid
-            key={movie.value}
-            selected={selected}
-            setSelected={setSelected}
-            movie={movie}
-          />
-        ))}
+        <div className="right-grid">
+          {MOVIES.map((movie) => (
+            <MovieGrid
+              key={movie.value}
+              selected={selected}
+              setSelected={setSelected}
+              movie={movie}
+            />
+          ))}
+        </div>
+        <button onClick={handleNext} className="next">Next Page</button>
       </div>
-      <button className="next">Next Page</button>
-      </div>
-      
+
     </div>
   );
 }

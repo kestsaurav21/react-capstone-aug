b89ad73128b76b1dece7f4949b61d32c3d37ca9b
ishar19 <isharjain19@gmail.com>
2024-12-06 22:22:14 +0530

    feat: weather and news api

--- src/pages/Widgets.jsx
+++ src/pages/Widgets.jsx
@@ -1,11 +1,15 @@
 import UserInfo from "../components/userInfo";
 import Note from "../components/note";
+import News from "../components/news";
+import Weather from "../components/weather";
 export default function Widgets() {
     return (
         <div>
             <h1>Widgets</h1>
             <UserInfo />
             <Note />
+            <News />
+            <Weather />
         </div>
     );
 }
